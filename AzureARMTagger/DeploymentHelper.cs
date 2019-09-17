a520a86e131d25345b7e3c3c7d0bb021cc10ac4d
Andrew Schmidt <Andrew.mkniger@gmail.com>
2019-09-17 15:50:27 -0500

    first development

---
+++ AzureARMTagger/DeploymentHelper.cs
@@ -0,0 +1,143 @@
+using System.Collections.Generic;
+// Requires the following Azure NuGet packages and related dependencies:
+// package id="Microsoft.Azure.Management.Authorization" version="2.0.0"
+// package id="Microsoft.Azure.Management.ResourceManager" version="1.4.0-preview"
+// package id="Microsoft.Rest.ClientRuntime.Azure.Authentication" version="2.2.8-preview"
+  
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Rest.Azure.Authentication;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace ARMHelpers
+{
+    /// <summary>
+    /// This is a helper class for deploying an Azure Resource Manager template
+    /// More info about template deployments can be found here https://go.microsoft.com/fwLink/?LinkID=733371
+    /// </summary>
+    class DeploymentHelper
+    {
+        string subscriptionId; 
+        string clientId; 
+        string clientSecret; 
+        string resourceGroupName; 
+        string deploymentName; 
+        string resourceGroupLocation; 
+        string tenantId;
+
+        public DeploymentHelper(string subscriptionId, string clientId, string clientSecret, string resourceGroupName, string deploymentName, string resourceGroupLocation, string tenantId){
+            this.subscriptionId = subscriptionId;
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+            this.resourceGroupName = resourceGroupName;
+            this.deploymentName = deploymentName;
+            this.resourceGroupLocation = resourceGroupLocation;
+            this.tenantId = tenantId;
+        }
+        public async void Run(string pathToTemplateFile, string pathToParameterFile, string pathToTagFile)
+        {
+            // Try to obtain the service credentials
+            var serviceCreds = await ApplicationTokenProvider.LoginSilentAsync(tenantId, clientId, clientSecret);
+
+            // Read the template and parameter file contents
+            JObject templateFileContents = GetJsonFileContents(pathToTemplateFile);
+            JObject parameterFileContents = GetJsonFileContents(pathToParameterFile);
+            
+            // Read the tags
+            JObject tagFileContents = GetJsonFileContents(pathToTagFile);
+            // Add Tags
+            templateFileContents = AddTagsToTemplate(templateFileContents, tagFileContents);
+
+            // Create the resource manager client
+            var resourceManagementClient = new ResourceManagementClient(serviceCreds);
+            resourceManagementClient.SubscriptionId = subscriptionId;
+
+            // Create or check that resource group exists
+            EnsureResourceGroupExists(resourceManagementClient, resourceGroupName, resourceGroupLocation);
+
+            // Start a deployment
+            DeployTemplate(resourceManagementClient, resourceGroupName, deploymentName, templateFileContents, parameterFileContents);
+        }
+
+        /// <summary>
+        /// Reads a JSON file from the specified path
+        /// </summary>
+        /// <param name="pathToJson">The full path to the JSON file</param>
+        /// <returns>The JSON file contents</returns>
+        private JObject GetJsonFileContents(string pathToJson)
+        {
+            JObject templatefileContent = new JObject();
+            using (StreamReader file = File.OpenText(pathToJson))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    templatefileContent = (JObject)JToken.ReadFrom(reader);
+                    return templatefileContent;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Iterates through resources in a template and adds tags to the resources
+        /// </summary>
+        /// <param name="template">JObject of the template</param>
+        /// <param name="tags">JObject of the tags</param>
+        /// <returns>The JSON Object with tags inserted</returns>
+        private JObject AddTagsToTemplate(JObject template, JObject tags){
+            foreach(JObject resource in template["resources"]){
+                foreach(var tag in tags){
+                    resource["tags"][tag.Key] = tag.Value;
+                }
+            }
+            return template;
+        }
+
+        /// <summary>
+        /// Ensures that a resource group with the specified name exists. If it does not, will attempt to create one.
+        /// </summary>
+        /// <param name="resourceManagementClient">The resource manager client.</param>
+        /// <param name="resourceGroupName">The name of the resource group.</param>
+        /// <param name="resourceGroupLocation">The resource group location. Required when creating a new resource group.</param>
+        private static void EnsureResourceGroupExists(ResourceManagementClient resourceManagementClient, string resourceGroupName, string resourceGroupLocation)
+        {
+            if (resourceManagementClient.ResourceGroups.CheckExistence(resourceGroupName) != true)
+            {
+                Console.WriteLine(string.Format("Creating resource group '{0}' in location '{1}'", resourceGroupName, resourceGroupLocation));
+                var resourceGroup = new ResourceGroup();
+                resourceGroup.Location = resourceGroupLocation;
+                resourceManagementClient.ResourceGroups.CreateOrUpdate(resourceGroupName, resourceGroup);
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Using existing resource group '{0}'", resourceGroupName));
+            }
+        }
+
+        /// <summary>
+        /// Starts a template deployment.
+        /// </summary>
+        /// <param name="resourceManagementClient">The resource manager client.</param>
+        /// <param name="resourceGroupName">The name of the resource group.</param>
+        /// <param name="deploymentName">The name of the deployment.</param>
+        /// <param name="templateFileContents">The template file contents.</param>
+        /// <param name="parameterFileContents">The parameter file contents.</param>
+        private static void DeployTemplate(ResourceManagementClient resourceManagementClient, string resourceGroupName, string deploymentName, JObject templateFileContents, JObject parameterFileContents)
+        {
+            Console.WriteLine(string.Format("Starting template deployment '{0}' in resource group '{1}'", deploymentName, resourceGroupName));
+            var deployment = new Deployment();
+
+            deployment.Properties = new DeploymentProperties
+            {
+                Mode = DeploymentMode.Incremental,
+                Template = templateFileContents,
+                Parameters = parameterFileContents["parameters"].ToObject<JObject>()
+            };
+
+            var deploymentResult = resourceManagementClient.Deployments.CreateOrUpdate(resourceGroupName, deploymentName, deployment);
+            Console.WriteLine(string.Format("Deployment status: {0}", deploymentResult.Properties.ProvisioningState));
+        }
+    }
+}
---
+++ AzureARMTagger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ARMHelpers;
+
+namespace AzureARMTagger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var deploymentHelper = new DeploymentHelper(
+                Environment.GetEnvironmentVariable("subscriptionId"), 
+                Environment.GetEnvironmentVariable("clientId"), 
+                Environment.GetEnvironmentVariable("clientSecret"), 
+                Environment.GetEnvironmentVariable("resourceGroupName"), 
+                Environment.GetEnvironmentVariable("deploymentName"), 
+                Environment.GetEnvironmentVariable("resourceGroupLocation"), 
+                Environment.GetEnvironmentVariable("tenantId")
+            );
+            string DirPath = Environment.GetEnvironmentVariable("pathToFiles")??"./";
+            
+            deploymentHelper.Run(DirPath+Environment.GetEnvironmentVariable("pathToTemplateFile"), DirPath+Environment.GetEnvironmentVariable("pathToParameterFile"), DirPath+Environment.GetEnvironmentVariable("pathToTagFile"));
+
+
+            
+
+
+        }
+    }
+}
