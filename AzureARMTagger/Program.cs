4f99cdc373d7cbaf400d775c6323b7e5ea32762e
Andrew <a90q9zz@mmm.com>
2019-09-18 08:31:08 -0500

    work

--- AzureARMTagger/Program.cs
+++ AzureARMTagger/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
-using ARMHelpers;
+﻿
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AzureARMTagger
 {
@@ -9,23 +12,49 @@ namespace AzureARMTagger
         {
             Console.WriteLine("Hello World!");
 
-            var deploymentHelper = new DeploymentHelper(
-                Environment.GetEnvironmentVariable("subscriptionId"), 
-                Environment.GetEnvironmentVariable("clientId"), 
-                Environment.GetEnvironmentVariable("clientSecret"), 
-                Environment.GetEnvironmentVariable("resourceGroupName"), 
-                Environment.GetEnvironmentVariable("deploymentName"), 
-                Environment.GetEnvironmentVariable("resourceGroupLocation"), 
-                Environment.GetEnvironmentVariable("tenantId")
-            );
             string DirPath = Environment.GetEnvironmentVariable("pathToFiles")??"./";
             
-            deploymentHelper.Run(DirPath+Environment.GetEnvironmentVariable("pathToTemplateFile"), DirPath+Environment.GetEnvironmentVariable("pathToParameterFile"), DirPath+Environment.GetEnvironmentVariable("pathToTagFile"));
 
-
+            // Read the template and parameter file contents
+            JObject templateFileContents = GetJsonFileContents(DirPath+Environment.GetEnvironmentVariable("pathToTemplateFile"));
             
-
-
+            // Read the tags
+            JObject tagFileContents = GetJsonFileContents(DirPath+Environment.GetEnvironmentVariable("pathToTagFile"));
+            // Add Tags
+            templateFileContents = AddTagsToTemplate(templateFileContents, tagFileContents);
         }
-    }
+
+        /// <summary>
+        /// Reads a JSON file from the specified path
+        /// </summary>
+        /// <param name="pathToJson">The full path to the JSON file</param>
+        /// <returns>The JSON file contents</returns>
+        static private JObject GetJsonFileContents(string pathToJson)
+        {
+            JObject templatefileContent = new JObject();
+            using (StreamReader file = File.OpenText(pathToJson))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    templatefileContent = (JObject)JToken.ReadFrom(reader);
+                    return templatefileContent;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Iterates through resources in a template and adds tags to the resources
+        /// </summary>
+        /// <param name="template">JObject of the template</param>
+        /// <param name="tags">JObject of the tags</param>
+        /// <returns>The JSON Object with tags inserted</returns>
+        static private JObject AddTagsToTemplate(JObject template, JObject tags){
+            foreach(JObject resource in template["resources"]){
+                foreach(var tag in tags){
+                    resource["tags"][tag.Key] = tag.Value;
+                }
+            }
+            return template;
+        }
 }
+    }
